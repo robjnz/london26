d6992eedb6685c0a778a0f7196fba936af5ee17c
robin harris <robjnz@outlook.com>
2019-05-23 00:44:22 +0100

    todays updates

--- components/Films/Avengers/AvengersC.vue
+++ components/Films/Avengers/AvengersC.vue
@@ -5,47 +5,202 @@
           src    = "https://www.youtube.com/embed/7nN6KVAgwK4"
           allowfullscreen
   ></b-embed>
+  
     <p class="title-color">
       Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle, remaining Avengers must figure out a way to bring back their vanquished allies for an epic showdown with Thanos.
-      </p>
+    </p>
+    
+      
 
 <h4 class="title-color">Cineworld</h4>
+<!--Bexleyheath-->
 <b-button-group>
 <Bexleyheathbutton />
-<Greenwichbutton />
-  </b-button-group>
+</b-button-group>
 <br><br>
+
+<!--<Monday />-->
+<!--<Tuesday />-->
+<!--<Wednesday />-->
+<Thursday />
+<Friday />
+<Saturday />
+<Sunday />
+
+<br><br>
+
+<!--Greenwich-->
+<b-button-group>
+<Greenwichbutton />
+</b-button-group>
+<br><br>
+
+<!--<Monday />-->
+<!--<Tuesday />-->
+<!--<Wednesday />-->
+<Thursday />
+<Friday />
+<Saturday />
+<Sunday />
+
+<br><br>
+  
+<!--Ilford -->
 <b-button-group>
 <Ilfordbutton />
+</b-button-group>
+<br><br>
+
+<!--<Monday />-->
+<!--<Tuesday />-->
+<!--<Wednesday />-->
+<Thursday />
+<Friday />
+<Saturday />
+<Sunday />
+
+<br><br>
+
+<!--west-->
+<b-button-group>
 <Westbutton />
   </b-button-group>
 
+  <br><br>
+
+<!--<Monday />-->
+<!--<Tuesday />-->
+<!--<Wednesday />-->
+<Thursday />
+<Friday />
+<Saturday />
+<Sunday />
+<br><br>
+
   <h4 class="title-color">Odeon</h4>
+  <!--Greenwich-->
   <b-button-group>
  <Ogreenwichbutton />
+ </b-button-group>
+  <br><br>
+
+  <!--<Monday />-->
+  <!--<Tuesday />-->
+  <!--<Wednesday />-->
+  <Thursday />
+  <Friday />
+  <Saturday />
+  <Sunday />
+  <br><br>
+
+<!--Southwoodford -->
+ <b-button-group>
   <Osouthbutton />
   </b-button-group>
+  <br><br>
+
+  <!--<Monday />-->
+  <!--<Tuesday />-->
+  <!--<Wednesday />-->
+  <Thursday />
+  <Friday />
+  <Saturday />
+  <Sunday />
+  <br><br>
 
   <h4 class="title-color">Picturehouses</h4>
+  <!--Greenwich -->
   <b-button-group>
   <Pgreenwichbutton />
-  <Phackneybutton />
   </b-button-group>
 <br><br>
+
+  <!--<Monday />-->
+  <!--<Tuesday />-->
+  <!--<Wednesday />-->
+  <Thursday />
+  <Friday />
+  <Saturday />
+  <!--<Sunday />-->
+  <br><br>
+
+<!--Hackney -->
+  <b-button-group>
+  <Phackneybutton />
+  </b-button-group>
+  
+<br><br>
+
+  <!--<Monday />-->
+  <!--<Tuesday />-->
+  <!--<Wednesday />-->
+  <Thursday />
+  <Friday />
+  <Saturday />
+  <Sunday />
+  <br><br>
+
+<!--stratford-->
   <b-button-group>
   <Pstratfordbutton />
   </b-button-group>
+  <br><br>
 
+  <!--<Monday />-->
+  <!--<Tuesday />-->
+  <!--<Wednesday />-->
+  <Thursday />
+  <Friday />
+  <Saturday />
+  <Sunday />
+  <br><br>
+  
   <h4 class="title-color">Showcase</h4>
+  <!--Newham-->
   <b-button-group>
   <Newhambutton />
   </b-button-group>
+  <br><br>
+
+  <!--<Monday />-->
+  <!--<Tuesday />-->
+  <!--<Wednesday />-->
+  <Thursday />
+  <Friday />
+  <Saturday />
+  <Sunday />
+  <br><br>
 
   <h4 class="title-color">Vue</h4>
+  <!--Romford -->
   <b-button-group>
   <Romfordbutton />
+  </b-button-group>
+  <br><br>
+
+  <!--<Monday />-->
+  <!--<Tuesday />-->
+  <!--<Wednesday />-->
+  <Thursday />
+  <Friday />
+  <Saturday />
+  <Sunday />
+  <br><br>
+  
+  <!--Stratford -->
+<b-button-group>
   <Vstratford />
   </b-button-group>
+  <br><br>
+
+  <!--<Monday />-->
+  <!--<Tuesday />-->
+  <!--<Wednesday />-->
+  <Thursday />
+  <Friday />
+  <Saturday />
+  <Sunday />
+  <br><br>
 
   <h4 class="title-color"></h4>
   <b-button-group>
@@ -73,6 +228,14 @@ import Vstratford from '@/components/Cinevue/Vstratford'
 import AvengersR from '@/components/Films/Avengers/AvengersR'
 import AvengersF from '@/components/Films/Avengers/AvengersF'
 import Nineteen from '@/components/Films/Year/Nineteen'
+import Monday from '@/components/Films/Days/Monday'
+import Tuesday from '@/components/Films/Days/Tuesday'
+import Wednesday from '@/components/Films/Days/Wednesday'
+import Thursday from '@/components/Films/Days/Thursday'
+import Friday from '@/components/Films/Days/Friday'
+import Saturday from '@/components/Films/Days/Saturday'
+import Sunday from '@/components/Films/Days/Sunday'
+
 
 
 
@@ -94,6 +257,13 @@ export default {
     AvengersR,
     AvengersF,
     Nineteen,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
 
       }
 }
